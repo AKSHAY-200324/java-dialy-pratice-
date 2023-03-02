4b0df47e757b89ca31b9aa397d1813ad699bd0d8
Akshay kumar <111167758+AKSHAY-200324@users.noreply.github.com>
2023-03-02 17:09:44 +0530

    Add files via upload
    pratice on 02-03-2023

---
+++ teamindia.java
@@ -0,0 +1,16 @@
+class team extends msdhoni
+{
+public static void main(String args[])
+{
+teamindia ti=new teamindia();
+ti.trophies(); 
+}
+public void trophies()
+{
+System.out.println(super.trophies);
+} 
+ } 
+class msdhoni
+{
+String trophies="ICC,worldcup";
+}
