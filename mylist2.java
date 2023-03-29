b6b3665848cf852e7b33b53606a60fb8189f6596
Akshay kumar <111167758+AKSHAY-200324@users.noreply.github.com>
2023-03-29 14:07:12 +0530

    pratice on 28-03-2023

---
+++ mylist2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.*;
+import java.util.*;
+import java.util.stream.*;
+class mylist3{
+
+	public static void main(String args[])
+	{
+		
+		ArrayList<Integer> al=new ArrayList<>();
+		
+		for(int i=1;i<=50;i++)
+			al.add(i);
+		
+	List<Integer> evenal=al.stream().filter(i->i%2==0)
+							.collect(Collectors.toList());
+		System.out.println(evenal);
+		
